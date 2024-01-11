008ba036c82fc465e1c3c47073c0c54c4c3d9881
GOD_OF_WOR <ayxanceferov2002ac@gmail.com>
2024-01-11 12:48:20 +0400

    sini

---
+++ mode.js
@@ -0,0 +1,24 @@
+const alt = "\r\n";
+let metin = "Sayta hos geldin"+alt
++"Bol bol hediye almagi unutma";
+alert(metin);
+
+let menu = document.querySelector(".menu");
+let menulst = document.querySelector(".menulst");
+
+// menu.addEventListener("click", () => {
+//     menulst.classList.toggle("showlst");
+//     console.log("hi");
+// });
+
+// let navbar1 = document.getElementById("navbar1");
+// let navbar2=document.getElementById("navbar2")
+// window.addEventListener("scroll", function() {
+//     if (window.scrollY > 100) {
+//         navbar1.classList.add("scrolled");
+//         navbar2.classList.add("scrolled")
+//     } else {
+//         navbar1.classList.remove("scrolled");
+//         navbar2.classList.remove("scrolled")
+//     }
+// });
